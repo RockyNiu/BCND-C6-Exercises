4c49392e73e8ff4657599d0ac6cf7afce1e89ec9
Lei Niu <lei.niu.ny@gmail.com>
2023-04-23 21:29:42 -0400

    Update fetchFlightStatus()

--- contracts/ExerciseC6D/ExerciseC6D.sol
+++ contracts/ExerciseC6D/ExerciseC6D.sol
@@ -138,9 +138,8 @@ contract ExerciseC6D {
                         external
     {
         // Generate a number between 0 - 9 to determine which oracles may respond
-
         // CODE EXERCISE 2: Replace the hard-coded value of index with a random index based on the calling account
-        uint8 index = 0;  /* Replace code here */
+        uint8 index = getRandomIndex(msg.sender);  /* Replace code here */
 
 
         // Generate a unique key for storing the request
@@ -151,6 +150,7 @@ contract ExerciseC6D {
                                             });
 
         // CODE EXERCISE 2: Notify oracles that match the index value that they need to fetch flight status
+        emit OracleRequest(index, flight, timestamp);
         /* Enter code here */
 
     }
